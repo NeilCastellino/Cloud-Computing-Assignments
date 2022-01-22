9d4c26d1eef374d0d01fc18493fdd5c8dc4b17c9
Neil Castellino <neilcastellino@gmail.com>
2022-01-21 19:50:28 -0800

    Modified Python Scripts

--- assn2/client.py
+++ assn2/client.py
@@ -1,8 +1,8 @@
 import socket
 import os
-import hashlib
+import sys
 
-server_ip = "172.17.147.1"
+server_ip = sys.argv[0]#"172.17.147.1"
 server_port = 5002
 separator = "<#1234>"
 
@@ -13,7 +13,7 @@ print(f"Connected successfully to server ({server_ip}, {server_port})")
 
 # Receive the file sent by the server
 received = s.recv(1024).decode("utf-8")
-filename, received_hash = received.split(separator)
+filename, file_size = received.split(separator)
 filename = os.path.basename(filename)
 
 # Save the file locally
@@ -22,19 +22,9 @@ with open(filename, "wb") as f:
         bytes_read = s.recv(1024)
         if not bytes_read:
             break
-        file_hash = hashlib.sha256(bytes_read).hexdigest()
         f.write(bytes_read)
 
-os.rename(filename, "mydata_client_copy.txt")
 print(f"File {filename} received successfully")
-
-#Hash verification
-print(f"Received Hash:  {received_hash}")
-print(f"Generated Hash: {file_hash}")
-if file_hash==received_hash:
-    print("Hash Verified")
-else:
-    print("Incorrect Hash value")
 
 #Close the socket
 s.close()
--- assn2/server.py
+++ assn2/server.py
@@ -1,16 +1,10 @@
 import socket
 import os
-import hashlib
 
-server_filename = "mydata.txt"
+server_filename = "server_storage/mydata.txt"
 separator = "<#1234>"
 server_ip = socket.gethostbyname(socket.gethostname())
 server_port = 5002
-
-#Create a file locally and write data to it
-f = open(server_filename, "w")
-f.write("Demo file by Neil Castellino")
-f.close()
 filesize = os.path.getsize(server_filename)
 
 #Create a socket connection and start listening for requests
@@ -23,16 +17,8 @@ print(f"Server is up and running on ({server_ip}, {server_port})")
 client_socket, address = s.accept()
 print(f"Client: ({address[0]}) is connected.")
 
-#Compute Hash
-with open(server_filename, "rb") as f:
-    while True:
-        bytes_read = f.read(1024)
-        if not bytes_read:
-            break
-        file_hash = hashlib.sha256(bytes_read).hexdigest()
-
 #Send the text file
-client_socket.send(f"{server_filename}{separator}{file_hash}".encode("utf-8"))
+client_socket.send(f"{server_filename}{separator}{file_size}".encode("utf-8"))
 with open(server_filename, "rb") as f:
     while True:
         bytes_read = f.read(1024)
@@ -41,7 +27,6 @@ with open(server_filename, "rb") as f:
         client_socket.sendall(bytes_read)
 
 print(f"File {server_filename} sent successfully")
-print(f"Hash: {file_hash}")
 
 #Close all sockets
 client_socket.close()
